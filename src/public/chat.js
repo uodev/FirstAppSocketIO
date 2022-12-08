929972dcb66705b1b6087009bb9508efef0b8f3f
Uygar Öztürk Ceylan <89963503+uodev@users.noreply.github.com>
2022-12-08 15:37:27 +0300

    change chat.js

--- src/public/chat.js
+++ src/public/chat.js
@@ -1,4 +1,4 @@
-const socket = io.connect("http://localhost:3000");
+const socket = io.connect("https://firstchatappsocket.onrender.com");
 
 const sender = document.getElementById("sender");
 const message = document.getElementById("message");
